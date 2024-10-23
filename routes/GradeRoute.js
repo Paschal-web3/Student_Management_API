0f9f551e008b0238008f09e9bb0b768fc842264b
paschal <chijinduihejirika4@gmail.com>
2024-10-23 14:42:45 +0100

    Grade Route

--- routes/GradeRoute.js
+++ routes/GradeRoute.js
@@ -2,9 +2,9 @@
 
 const GradeController = require ('../controller/GradeControl')
 const route = require ('express').Router();
-const verify = require ('../middleware/Auth')
+// const verify = require ('../middleware/Auth')
 
 route.post('/addGrade' , GradeController.addGrade)
-route.get('/getGrade',verify, GradeController.getGrade)
+route.get('/getGrade', GradeController.getGrade)
 
 module.exports = route;
